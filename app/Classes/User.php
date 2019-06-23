0a99cf9206385710bc7bc5beff652749ad0b8b7f
Peanut <john1993228@gmail.com>
2019-06-23 22:20:47 +0800

    Create User Class

---
+++ app/Classes/User.php
@@ -0,0 +1,142 @@
+<?php
+namespace App\Classes;
+
+class User 
+{
+    private $ixUser = 0;
+    private $sUsername = '';
+    private $sPassword;
+    private $iCreatedTimestamp = null;
+    private $iUpdatedTimestamp = null;
+
+    /**
+     * 設定使用者編號
+     *
+     * @param int $id
+     * @return void
+     */
+    public function setId(int $id) : void
+    {
+        $this->ixUser = $id;
+    }
+
+    /**
+     * 設定使用者名稱
+     *
+     * @param string $username
+     * @return void
+     */
+    public function setUsername(string $username) : void
+    {
+        $this->sUsername = $username;
+    }
+
+    /**
+     * 設定使用者密碼
+     *
+     * @param string $password
+     * @return void
+     */
+    public function setEncryptedPassword(string $password) : void
+    {
+        $this->sPassword = $password;
+    }
+
+    /**
+     * 設定建立時間
+     *
+     * @param int $createdTimestamp
+     * @return void
+     */
+    public function setCreatedTimestamp(int $createdTimestamp) : void
+    {
+        $this->iCreatedTimestamp = $createdTimestamp;
+    }
+
+    /**
+     * 設定更新時間
+     *
+     * @param int $createdTimestamp
+     * @return void
+     */
+    public function setUpdatedTimestamp(int $updateTimestamp) : void
+    {
+        $this->iUpdatedTimestamp = $updateTimestamp;
+    }
+
+    /**
+     * 取得使用者編號
+     *
+     * @return int
+     */
+    public function getId() : int
+    {
+        return $this->ixUser;
+    }
+
+    /**
+     * 取得使用者名稱
+     *
+     * @return string
+     */
+    public function getUsername() : string
+    {
+        return $this->sUsername;
+    }
+
+    /**
+     * 檢查碼是否正確
+     *
+     * @param string $password
+     * @return bool
+     */
+    public function isPasswordCorrect(string $password) : bool
+    {
+        return password_verify($password, $this->sPassword);
+    }
+
+    /**
+     *  取得使用者建立時間
+     *
+     * @return int | null
+     */
+    public function getCreatedTimestamp()
+    {
+        return $this->iCreatedTimestamp;
+    }
+
+    /**
+     *  取得使用者更新時間
+     *
+     * @return int | null
+     */
+    public function getUpdatedTimestamp()
+    {
+        return $this->iUpdatedTimestamp; 
+    }
+
+    /**
+     * 透過陣列載入
+     *
+     * @param array $content
+     * @return void
+     */
+    public function loadFromArray(array $content) : void
+    {
+        if (isset($content['ixUser'])) {
+            $this->setId($content['ixUser']);
+        }
+        if (isset($content['sUsername'])) {
+            $this->setUsername($content['sUsername']);
+        }
+        if (isset($content['sPassword'])) {
+            $this->setEncryptedPassword($content['sPassword']);
+        }
+        if (isset($content['iCreatedTimestamp'])) {
+            $this->setCreatedTimestamp($content['iCreatedTimestamp']);
+        }
+        if (isset($content['iUpdatedTimestamp'])) {
+            $this->setUpdatedTimestamp($content['iUpdatedTimestamp']);
+        }
+    }
+}
---
+++ tests/Classes/UserTest.php
@@ -0,0 +1,75 @@
+<?php
+namespace Tests\Classes;
+
+use Tests\TestCase;
+use App\Classes\User;
+
+class UserTest extends TestCase
+{
+    /**
+     * 測試setter and getter
+     *
+     * @return void
+     */
+    public function testSetterAndGetter()
+    {
+        $user = new User();
+        $user->setId(1);
+        $user->setUsername('Jerry');
+        $user->setEncryptedPassword('$2y$10$E0OoZlDLyaGyIReyP.tnIuhs96/txzMGpQcb6JmIWumf/THLg9Ih2');
+        $user->setCreatedTimestamp(123456789);
+        $user->setUpdatedTimestamp(123456789);
+
+        $this->assertEquals(1, $user->getId());
+        $this->assertEquals('Jerry', $user->getUsername());
+        $this->assertEquals(123456789, $user->getCreatedTimestamp());
+        $this->assertEquals(123456789, $user->getUpdatedTimestamp());
+    }
+
+    /**
+     * 測試從陣列載入
+     *
+     * @return void
+     */
+    public function testLoadFromArray()
+    {
+        $content['ixUser'] = 99;
+        $content['sUsername'] = 'Jerry';
+        $content['sPassword'] = '$2y$10$E0OoZlDLyaGyIReyP.tnIuhs96/txzMGpQcb6JmIWumf/THLg9Ih2';
+        $content['iCreatedTimestamp'] = 123456789;
+        $content['iUpdatedTimestamp'] = 123456789;
+
+        $user = new User();
+        $user->loadFromArray($content);
+        $this->assertEquals(99, $user->getId());
+        $this->assertEquals('Jerry', $user->getUsername());
+        $this->assertEquals(123456789, $user->getCreatedTimestamp());
+        $this->assertEquals(123456789, $user->getUpdatedTimestamp());
+    }
+
+    /**
+     * 測試檢查密碼是否正確
+     *
+     * @return void
+     */
+    public function testIsPasswordCorrect()
+    {
+        $user = new User();
+        $user->setEncryptedPassword('$2y$10$.IukgQA8BibAzmHNe01U2eJUpaseXNsemWXRdM5GXTFyJxShzSvou');
+        $result = $user->isPasswordCorrect('123456789');
+        $this->assertTrue($result);
+    }
+
+    /**
+     * 測試檢查密碼是否正確
+     *
+     * @return void
+     */
+    public function testIsPasswordIfIncorrect()
+    {
+        $user = new User();
+        $user->setEncryptedPassword('$2y$10$.IukgQA8BibAzmHNe01U2eJUpaseXNsemWXRdM5GXTFyJxShzSvou');
+        $result = $user->isPasswordCorrect('12345678');
+        $this->assertFalse($result);
+    }
+}
